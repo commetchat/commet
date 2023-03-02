e72914cc1b85bb2173540d0349bac60e95107e99
Airyz <36567925+Airyzz@users.noreply.github.com>
2023-03-03 08:15:21 +1030

    Update build_config.dart

--- commet/lib/config/build_config.dart
+++ commet/lib/config/build_config.dart
@@ -2,4 +2,10 @@ class BuildConfig {
   static const bool DEBUG = String.fromEnvironment('BUILD_MODE', defaultValue: "debug") == "debug";
 
   static const bool RELEASE = String.fromEnvironment('BUILD_MODE', defaultValue: "debug") == "release";
+
+  static const bool DESKTOP = String.fromEnvironment('PLATFORM', defaultValue: "desktop") == "desktop";
+
+  static const bool WEB = String.fromEnvironment('PLATFORM', defaultValue: "desktop") == "web";
+
+  static const bool MOBILE = String.fromEnvironment('PLATFORM', defaultValue: "desktop") == "mobile";
 }
