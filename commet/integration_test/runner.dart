754b0027fe86a283c96f7926ae0f31fb3b6ad5d5
Airyzz <36567925+Airyzz@users.noreply.github.com>
2023-04-09 18:57:08 +0930

    Update runner.dart

--- commet/integration_test/runner.dart
+++ commet/integration_test/runner.dart
@@ -3,10 +3,12 @@
 import 'package:integration_test/integration_test.dart';
 import 'matrix/login_test.dart' as login_test;
 import 'matrix/key_verification_test.dart' as key_verification_test;
+import 'matrix/create_space_test.dart' as create_space_test;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   login_test.main();
   key_verification_test.main();
+  create_space_test.main();
 }
