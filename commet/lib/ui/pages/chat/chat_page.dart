3ac2879fbc51d55f18e6645637aedd5ce3bbb184
Airyz <36567925+Airyzz@users.noreply.github.com>
2023-05-28 11:27:20 +0930

    Update chat_page.dart

--- commet/lib/ui/pages/chat/chat_page.dart
+++ commet/lib/ui/pages/chat/chat_page.dart
@@ -10,7 +10,6 @@ import 'package:commet/ui/pages/settings/room_settings_page.dart';
 import 'package:commet/utils/notification/notification_manager.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
-import 'package:tiamat/tiamat.dart';
 import 'package:tiamat/tiamat.dart' as tiamat;
 import '../../../client/attachment.dart';
 import '../../../client/client.dart';
