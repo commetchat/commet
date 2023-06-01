80dfa0e810448d874642bfb2a8cccc5604140e14
Gedeon Sainrival <hello@gedeondoescode.com>
2023-05-31 21:57:19 -0400

    holy shit it's so close to rendering them

--- commet/lib/ui/molecules/emoji_picker/emoji_picker.dart
+++ commet/lib/ui/molecules/emoji_picker/emoji_picker.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:tiamat/tiamat.dart';
 import 'package:commet/utils/emoji/emoji_pack.dart';
+import 'package:commet/ui/atoms/emoji_widget.dart';
 
-// Future<void> emojiPacks() {
-//   var packs = EmojiPack.defaults();
-//   return EmojiPickerWidget(packs);
-// }
+import 'package:widgetbook_annotation/widgetbook_annotation.dart';
+
+@WidgetbookUseCase(name: 'Emoji Picker', type: EmojiPickerWidget)
+@Deprecated("widgetbook")
+Widget emojiPickerWidget(BuildContext context) {
+  return SizedBox(
+      width: 100,
+      height: 100,
+      child: FutureBuilder(
+          future: EmojiPack.defaults(),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) =>
+              snapshot.hasData
+                  ? EmojiPickerWidget(snapshot.data as List<EmojiPack>)
+                  : const Center(
+                      child: CircularProgressIndicator(),
+                    )));
+}
 
 class EmojiPickerWidget extends StatelessWidget {
-  var emojiPacks = EmojiPack.defaults()
+  const EmojiPickerWidget(this.packs, {super.key});
+
+  final List<EmojiPack> packs;
 
   @override
   Widget build(BuildContext context) {
     return Panel(
-        child: ListView.builder(
-          itemCount: emojiPacks.length,
-    ));
+        child: SizedBox(
+            width: 90,
+            height: 84,
+            child: ListView.builder(
+              itemCount: packs.length,
+              itemBuilder: (BuildContext context, int packIndex) {
+                return GridView.count(
+                  crossAxisCount: 8,
+                  shrinkWrap: true,
+                  children: packs[packIndex]
+                      .emoji
+                      .map((e) => EmojiWidget(e))
+                      .toList(),
+                );
+              },
+            )));
   }
 }
