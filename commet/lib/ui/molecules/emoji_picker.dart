c4e919caf3adf8551120cb9dc21e29d8929a59cc
Airyz <36567925+Airyzz@users.noreply.github.com>
2023-06-11 19:54:43 +0930

    Update emoji_picker.dart

--- commet/lib/ui/molecules/emoji_picker.dart
+++ commet/lib/ui/molecules/emoji_picker.dart
@@ -1,3 +1,4 @@
+import 'package:commet/utils/emoji/emoticon.dart';
 import 'package:commet/utils/emoji/unicode_emoji.dart';
 import 'package:flutter/material.dart';
 import 'package:tiamat/tiamat.dart';
@@ -17,16 +18,24 @@ Widget wbEmojiPickerDefault(BuildContext context) {
           future: UnicodeEmojis.load(),
           builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) =>
               snapshot.hasData
-                  ? EmojiPicker(snapshot.data as List<EmoticonPack>)
+                  ? EmojiPicker(
+                      snapshot.data as List<EmoticonPack>,
+                      onEmoticonPressed: (emoticon) {
+                        // ignore: avoid_print
+                        print("Emoticon Clicked: ${emoticon.slug}");
+                      },
+                    )
                   : const Center(
                       child: CircularProgressIndicator(),
                     )));
 }
 
 class EmojiPicker extends StatelessWidget {
-  const EmojiPicker(this.packs, {super.key});
-
+  const EmojiPicker(this.packs,
+      {super.key, this.size = 38, this.onEmoticonPressed});
+  final void Function(Emoticon emoticon)? onEmoticonPressed;
   final List<EmoticonPack> packs;
+  final double size;
 
   @override
   Widget build(BuildContext context) {
@@ -36,27 +45,37 @@ class EmojiPicker extends StatelessWidget {
                 child: ListView.builder(
       itemCount: packs.length,
       itemBuilder: (BuildContext context, int packIndex) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            tiamat.Text.labelLow(packs[packIndex].displayName),
-            Wrap(
-              spacing: 5,
-              runSpacing: 10,
-              children: packs[packIndex]
-                  .emotes
-                  .map((e) => InkWell(
-                      onTap: () {},
-                      onHover: (value) {},
-                      mouseCursor: SystemMouseCursors.click,
-                      child: Padding(
-                          padding: const EdgeInsets.all(5.0),
-                          child: EmojiWidget(e))))
-                  .toList(),
-            )
-          ],
-        );
+        return buildListItem(packIndex);
       },
     ))));
+  }
+
+  Column buildListItem(int packIndex) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        tiamat.Text.labelLow(packs[packIndex].displayName),
+        Wrap(
+          alignment: WrapAlignment.start,
+          runSpacing: 1,
+          spacing: 1,
+          children:
+              packs[packIndex].emotes.map((e) => buildEmoticon(e)).toList(),
+        )
+      ],
+    );
+  }
+
+  Widget buildEmoticon(Emoticon emoticon) {
+    return SizedBox(
+        width: size,
+        height: size,
+        child: InkWell(
+            borderRadius: BorderRadius.circular(10),
+            onTap: () => onEmoticonPressed?.call(emoticon),
+            mouseCursor: SystemMouseCursors.click,
+            child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Center(child: EmojiWidget(emoticon)))));
   }
 }
