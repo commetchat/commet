e2990a53bf747874a4ee544a39ff33cc7bf0dce6
Airyz <36567925+Airyzz@users.noreply.github.com>
2023-08-27 21:56:37 +0930

    Start implementing invitations

--- commet/lib/ui/atoms/room_panel.dart
+++ commet/lib/ui/atoms/room_panel.dart
@@ -72,7 +72,7 @@ class _RoomPanelState extends State<RoomPanel> {
                                 nameAndTopic(),
                                 if (widget.recentEventBody != null &&
                                     widget.recentEventSender != null)
-                                  recentEvent()
+                                  Flexible(child: recentEvent())
                               ],
                             ),
                           ),
@@ -91,7 +91,7 @@ class _RoomPanelState extends State<RoomPanel> {
   }
 
   Widget recentEvent() {
-    return Column(
+    return Row(
       crossAxisAlignment: CrossAxisAlignment.baseline,
       mainAxisSize: MainAxisSize.min,
       textBaseline: TextBaseline.alphabetic,
@@ -100,14 +100,16 @@ class _RoomPanelState extends State<RoomPanel> {
           widget.recentEventSender!,
           type: TextType.labelLow,
           autoAdjustBrightness: true,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
           color: widget.recentEventSenderColor,
         ),
-        tiamat.Text.labelLow(
-          widget.recentEventBody!,
-          overflow: TextOverflow.ellipsis,
-          maxLines: 1,
+        const SizedBox(
+          width: 10,
+        ),
+        Flexible(
+          child: tiamat.Text.labelLow(
+            widget.recentEventBody!,
+            overflow: TextOverflow.fade,
+          ),
         )
       ],
     );
