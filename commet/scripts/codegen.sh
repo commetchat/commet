0194af959fb5bbbdd4379e9f921a0d6a098d534f
Airyzz <36567925+Airyzz@users.noreply.github.com>
2023-08-31 18:16:14 +0930

    Fix codegen

--- commet/scripts/codegen.sh
+++ commet/scripts/codegen.sh
@@ -5,7 +5,7 @@ flutter pub get
 flutter pub run intl_utils:generate
 
 # Generate translation files for use in actual code
-mkdir lib/generated/l10n
+mkdir -p lib/generated/l10n
 flutter pub run intl_translation:generate_from_arb $(find lib/ -name '*.dart') assets/l10n/*.arb --output-dir=lib/generated/l10n
 
 flutter pub run build_runner build
--- commet/scripts/generate-from-arb.sh
+++ commet/scripts/generate-from-arb.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-mkdir lib/generated/l10n
+mkdir -p lib/generated/l10n
 flutter pub run intl_translation:generate_from_arb $(find lib/ -name '*.dart') assets/l10n/*.arb --output-dir=lib/generated/l10n
