935ce8888b4442de36b5891730eff11976787724
Airyz <36567925+Airyzz@users.noreply.github.com>
2023-09-30 13:56:14 +0930

    Fixing size of join button in room panel

--- commet/lib/ui/atoms/room_panel.dart
+++ commet/lib/ui/atoms/room_panel.dart
@@ -8,7 +8,6 @@ class RoomPanel extends StatefulWidget {
   const RoomPanel(
       {required this.displayName,
       this.avatar,
-      this.topic,
       this.onPrimaryButtonPressed,
       this.primaryButtonLabel,
       this.primaryButtonLoading = false,
@@ -19,16 +18,15 @@ class RoomPanel extends StatefulWidget {
       this.showSettingsButton = false,
       this.onTap,
       this.color,
-      this.recentEventBody,
+      this.body,
       this.recentEventSender,
       this.recentEventSenderColor,
       super.key});
   final ImageProvider? avatar;
   final String displayName;
-  final String? topic;
   final bool showSettingsButton;
   final Color? color;
-  final String? recentEventBody;
+  final String? body;
   final String? recentEventSender;
   final Color? recentEventSenderColor;
   final String? primaryButtonLabel;
@@ -57,7 +55,8 @@ class _RoomPanelState extends State<RoomPanel> {
       !showSecondaryButton &&
       widget.showSettingsButton != true;
 
-  bool get showAnyButton => showPrimaryButton || showSecondaryButton;
+  bool get showAnyButton =>
+      showPrimaryButton || showSecondaryButton || widget.showSettingsButton;
 
   @override
   Widget build(BuildContext context) {
@@ -75,11 +74,12 @@ class _RoomPanelState extends State<RoomPanel> {
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.min,
                     children: [
-                      Flexible(
+                      Expanded(
                         child: Row(
                           crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
+                          mainAxisSize: MainAxisSize.max,
                           children: [
                             Avatar.medium(
                               image: widget.avatar,
@@ -91,13 +91,16 @@ class _RoomPanelState extends State<RoomPanel> {
                                 padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
                                 child: Column(
                                   mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    nameAndTopic(),
-                                    if (widget.recentEventBody != null &&
-                                        widget.recentEventSender != null)
-                                      recentEvent()
+                                    tiamat.Text.labelEmphasised(
+                                      widget.displayName,
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    if (widget.body != null)
+                                      Flexible(child: recentEvent())
                                   ],
                                 ),
                               ),
@@ -105,15 +108,10 @@ class _RoomPanelState extends State<RoomPanel> {
                           ],
                         ),
                       ),
-                      if (BuildConfig.DESKTOP && showOnlyPrimaryButton)
-                        Flexible(
-                            child: actionButtons(widget.showSettingsButton)),
-                      if (BuildConfig.MOBILE && widget.showSettingsButton)
-                        settingsButton(),
+                      if (showAnyButton)
+                        actionButtons(widget.showSettingsButton),
                     ],
                   ),
-                  if (BuildConfig.MOBILE && !showOnlyPrimaryButton)
-                    actionButtons(false)
                 ],
               ),
             ),
@@ -129,16 +127,17 @@ class _RoomPanelState extends State<RoomPanel> {
       mainAxisSize: MainAxisSize.min,
       textBaseline: TextBaseline.alphabetic,
       children: [
-        tiamat.Text(
-          widget.recentEventSender!,
-          type: TextType.labelLow,
-          autoAdjustBrightness: true,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          color: widget.recentEventSenderColor,
-        ),
+        if (widget.recentEventSender != null)
+          tiamat.Text(
+            widget.recentEventSender!,
+            type: TextType.labelLow,
+            autoAdjustBrightness: true,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            color: widget.recentEventSenderColor,
+          ),
         tiamat.Text.labelLow(
-          widget.recentEventBody!,
+          widget.body!,
           overflow: TextOverflow.ellipsis,
           maxLines: 1,
         )
@@ -148,20 +147,18 @@ class _RoomPanelState extends State<RoomPanel> {
 
   Widget actionButtons(bool includeSettings) {
     return Row(
-      mainAxisSize: MainAxisSize.max,
+      mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         if (showPrimaryButton)
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
-              child: Button(
-                text: widget.primaryButtonLabel!,
-                isLoading: widget.primaryButtonLoading,
-                onTap: () {
-                  widget.onPrimaryButtonPressed?.call();
-                },
-              ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+            child: Button(
+              text: widget.primaryButtonLabel!,
+              isLoading: widget.primaryButtonLoading,
+              onTap: () {
+                widget.onPrimaryButtonPressed?.call();
+              },
             ),
           ),
         if (showSecondaryButton)
@@ -187,22 +184,6 @@ class _RoomPanelState extends State<RoomPanel> {
       icon: Icons.settings,
       radius: BuildConfig.MOBILE ? 24 : 16,
       onPressed: widget.onRoomSettingsButtonPressed,
-    );
-  }
-
-  Widget nameAndTopic() {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        tiamat.Text.labelEmphasised(widget.displayName),
-        if (widget.topic != null)
-          const SizedBox(
-            width: 20,
-            child: Seperator(),
-          ),
-        if (widget.topic != null) tiamat.Text.body(widget.topic!),
-      ],
     );
   }
 }
