dbdee94fdedba7a430ec61cbd4fdb281b8628b45
Airyz <36567925+Airyzz@users.noreply.github.com>
2023-11-21 16:21:29 +1030

    Dont suppress active room if in backgrround

--- commet/lib/utils/notification/modifiers/suppress_active_room.dart
+++ commet/lib/utils/notification/modifiers/suppress_active_room.dart
@@ -1,6 +1,7 @@
 import 'package:commet/utils/event_bus.dart';
 import 'package:commet/utils/notification/modifiers/notification_modifiers.dart';
 import 'package:commet/utils/notification/notification_content.dart';
+import 'package:flutter/material.dart';
 
 class NotificationModifierSuppressActiveRoom implements NotificationModifier {
   String roomId = "";
@@ -13,6 +14,11 @@ class NotificationModifierSuppressActiveRoom implements NotificationModifier {
 
   @override
   Future<NotificationContent?> process(NotificationContent content) async {
+    // Dont do this if we are in the background!
+    if (WidgetsBinding.instance.lifecycleState != AppLifecycleState.resumed) {
+      return content;
+    }
+
     if (content is! MessageNotificationContent) return content;
 
     if (content.roomId == roomId) return null;
