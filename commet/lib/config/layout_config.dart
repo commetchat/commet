f5dfebccccec805bdac68b6891be34d37b990397
Airyz <36567925+Airyzz@users.noreply.github.com>
2023-12-26 10:46:42 +1030

    Update layout_config.dart

--- commet/lib/config/layout_config.dart
+++ commet/lib/config/layout_config.dart
@@ -7,12 +7,20 @@ class Layout {
       return true;
     }
 
+    if (BuildConfig.MOBILE) {
+      return false;
+    }
+
     return !_isPortrait();
   }
 
   static bool get mobile {
     if (BuildConfig.MOBILE) {
       return true;
+    }
+
+    if (BuildConfig.DESKTOP) {
+      return false;
     }
 
     return _isPortrait();
