89861f5de8983b8241c0158415572d8afdc3aa7d
Airyz <36567925+Airyzz@users.noreply.github.com>
2024-06-16 15:47:32 +0930

    start creating ui for sub spaces

---
+++ tiamat/lib/config/style/theme_base.dart
@@ -0,0 +1,39 @@
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:tiamat/config/style/theme_common.dart';
+import 'package:tiamat/config/style/theme_extensions.dart';
+import 'dart:io' show Platform;
+
+class ThemeBase {
+  static ThemeData theme(Brightness brightness) => ThemeData(
+        fontFamily: "RobotoCustom",
+        fontFamilyFallback: ThemeCommon.fontFamilyFallback(),
+        brightness: brightness,
+        useMaterial3: true,
+        expansionTileTheme: ExpansionTileThemeData(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            collapsedShape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            tilePadding: EdgeInsets.fromLTRB(8, 0, 8, 0)),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+                shape: WidgetStatePropertyAll(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        ))),
+        textButtonTheme: TextButtonThemeData(
+            style: ButtonStyle(
+          padding: WidgetStatePropertyAll<EdgeInsetsGeometry?>(
+              EdgeInsets.fromLTRB(8, 0, 8, 0)),
+          shape: WidgetStatePropertyAll<OutlinedBorder>(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+          textStyle: const WidgetStatePropertyAll<TextStyle>(
+            TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        )),
+      );
+}
