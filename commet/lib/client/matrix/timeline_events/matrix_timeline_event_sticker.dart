ddd6279a9b54ebb8346a5088412494d000e164eb
Airyzz <36567925+Airyzz@users.noreply.github.com>
2024-08-30 22:29:35 +0000

    Fix reactions not rendering on sticker events (#349)

--- commet/lib/client/matrix/timeline_events/matrix_timeline_event_sticker.dart
+++ commet/lib/client/matrix/timeline_events/matrix_timeline_event_sticker.dart
@@ -1,11 +1,12 @@
 import 'package:commet/client/matrix/matrix_mxc_image_provider.dart';
 import 'package:commet/client/matrix/timeline_events/matrix_timeline_event.dart';
+import 'package:commet/client/matrix/timeline_events/matrix_timeline_event_mixin_reactions.dart';
 import 'package:commet/client/matrix/timeline_events/matrix_timeline_event_mixin_related.dart';
 import 'package:commet/client/timeline_events/timeline_event_sticker.dart';
 import 'package:flutter/material.dart';
 
 class MatrixTimelineEventSticker extends MatrixTimelineEvent
-    with MatrixTimelineEventRelated
+    with MatrixTimelineEventRelated, MatrixTimelineEventReactions
     implements TimelineEventSticker {
   MatrixTimelineEventSticker(super.event, {required super.client}) {
     stickerImage = MatrixMxcImage(
