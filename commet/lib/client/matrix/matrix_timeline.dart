35ae8c0fef1c1827000988df2eee95339d6c79cb
Airyz <36567925+Airyzz@users.noreply.github.com>
2024-10-08 22:25:58 +1030

    Update matrix_timeline.dart

--- commet/lib/client/matrix/matrix_timeline.dart
+++ commet/lib/client/matrix/matrix_timeline.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:commet/client/components/emoticon/emoticon.dart';
+import 'package:commet/client/matrix/matrix_client.dart';
 import 'package:commet/client/matrix/matrix_room.dart';
 import 'package:commet/client/matrix/timeline_events/matrix_timeline_event.dart';
 import 'package:commet/client/timeline_events/timeline_event.dart';
