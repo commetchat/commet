75aa8f775fdbb0dd29f2aef9e0613cecdeb19781
Airyzz <36567925+Airyzz@users.noreply.github.com>
2025-02-21 22:54:38 +1030

    fix MultiDatabaseServer not working across activities

--- commet/lib/utils/database/multiple_database_server.dart
+++ commet/lib/utils/database/multiple_database_server.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:isolate';
 import 'dart:ui';
 
+import 'package:commet/debug/log.dart';
 import 'package:drift/drift.dart';
 import 'package:drift/isolate.dart';
 import 'package:drift/native.dart';
@@ -12,19 +13,40 @@ final class MultiDatabaseServer {
 
   MultiDatabaseServer() {
     _receiveConnections.listen((message) {
-      if (message is (String, SendPort)) {
-        final name = message.$1;
-        final isolate = _activeIsolates.putIfAbsent(
-          name,
-          () {
-            return DriftIsolate.inCurrent(
-                // obviously you can pass a path instead of a name and use that to open the right NativeDatabase
-                () => NativeDatabase(File(name)));
-          },
-        );
-
-        message.$2.send(isolate.connectPort);
+      if (message is! List) {
+        Log.e("Expected to receive a list");
+        return;
       }
+
+      if (message.length != 2) {
+        Log.e("Received list of incorrect length");
+        return;
+      }
+
+      final name = message[0];
+      final port = message[1];
+
+      if (name is! String) {
+        Log.e("list[0] was not a string");
+        return;
+      }
+
+      if (port is! SendPort) {
+        Log.e("list[1] was not a SendPort");
+        return;
+      }
+
+      final isolate = _activeIsolates.putIfAbsent(
+        name,
+        () {
+          return DriftIsolate.inCurrent(
+              serialize: true,
+              // obviously you can pass a path instead of a name and use that to open the right NativeDatabase
+              () => NativeDatabase(File(name)));
+        },
+      );
+
+      port.send(isolate.connectPort);
     });
   }
 }
@@ -52,11 +74,15 @@ class DatabaseIsolate {
   }
 
   static Future<DatabaseConnection> connect(String databaseName) async {
+    if (connectToServer == null) {
+      await start();
+    }
+
     final response = ReceivePort();
-    connectToServer!.send((databaseName, response.sendPort));
+
+    connectToServer!.send([databaseName, response.sendPort]);
 
     final connectPort = await response.first as SendPort;
-    return DriftIsolate.fromConnectPort(connectPort, serialize: false)
-        .connect();
+    return DriftIsolate.fromConnectPort(connectPort, serialize: true).connect();
   }
 }
