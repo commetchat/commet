9509b2953cfcb78f0073699bb6e94edb7e003e44
Brian Quirt <polemarkh@gmail.com>
2025-03-09 12:51:25 -0400

    Ready to start testing ios build action

--- commet/ios/Runner/AppDelegate.swift
+++ commet/ios/Runner/AppDelegate.swift
@@ -1,5 +1,9 @@
 import UIKit
 import Flutter
+import Firebase
+import FirebaseCore
+import flutter_local_notifications
+import flutter_background_service_ios
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +12,16 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+	if #available(iOS 10.0, *) {
+	  UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+	}
+	FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+      GeneratedPluginRegistrant.register(with: registry)
+    }
+    SwiftFlutterBackgroundServicePlugin.taskIdentifier = "dev.flutter.background.refresh"
+//    FirebaseApp.configure()
+//    GeneratedPluginRegistrant.register(with: self)
+    
+	return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
