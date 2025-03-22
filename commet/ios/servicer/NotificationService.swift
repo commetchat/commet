497a057be007f69ab88b193a856f7bbceac36da1
Brian Quirt <polemarkh@gmail.com>
2025-03-22 15:10:35 -0400

    Removed flutter framework from extension

--- commet/ios/servicer/NotificationService.swift
+++ commet/ios/servicer/NotificationService.swift
@@ -6,9 +6,6 @@
 //
 
 import UserNotifications
-import Flutter
-
-let channelName : String = "PushServiceChannel"
 
 class NotificationService: UNNotificationServiceExtension {
 
@@ -18,8 +15,6 @@ class NotificationService: UNNotificationServiceExtension {
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        var flutterEngine : FlutterEngine? = FlutterEngine(name: "notification_service_engine", project: nil, allowHeadlessExecution: true)
-        let decodeChannel = FlutterMethodChannel(name: channelName, binaryMessenger: flutterEngine!.binaryMessenger)
 
         if let bestAttemptContent = bestAttemptContent {
             var displayRoom : String = ""
@@ -36,22 +31,6 @@ class NotificationService: UNNotificationServiceExtension {
             bestAttemptContent.subtitle = "\(unreadCount) new message(s)"
             bestAttemptContent.body = "Tap on this message to be taken to the room where it happened™️"
 
-//            do {
-//                flutterEngine!.run(withEntrypoint: "notificationMutationService")
-//                DispatchQueue.main.sync() {
-//                
-//                    decodeChannel.invokeMethod("notificationReceived", arguments: bestAttemptContent.userInfo, result:{(result) -> () in
-//                        let result = result as! NSDictionary
-//                        
-//                        bestAttemptContent.title = result["title"] as! String
-//                        bestAttemptContent.subtitle = result["subtitle"] as! String
-//                        bestAttemptContent.body = result["body"] as! String
-//                    })
-//               
-//                }
-//            } catch let error {
-//                bestAttemptContent.subtitle = "Error: \(error)"
-//            }
             contentHandler(bestAttemptContent)
         }
     }
