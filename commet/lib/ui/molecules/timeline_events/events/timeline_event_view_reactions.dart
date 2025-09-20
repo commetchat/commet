83541e716ce94f269d2c38b3ae5d690d794f3213
Airyzz <36567925+Airyzz@users.noreply.github.com>
2025-09-21 01:15:05 +0930

    Long press to see which users reacted to a message (#463)

--- commet/lib/ui/molecules/timeline_events/events/timeline_event_view_reactions.dart
+++ commet/lib/ui/molecules/timeline_events/events/timeline_event_view_reactions.dart
@@ -3,7 +3,10 @@ import 'package:commet/client/timeline.dart';
 import 'package:commet/client/timeline_events/timeline_event.dart';
 import 'package:commet/client/timeline_events/timeline_event_feature_reactions.dart';
 import 'package:commet/ui/atoms/emoji_reaction.dart';
+import 'package:commet/ui/atoms/emoji_widget.dart';
 import 'package:commet/ui/molecules/timeline_events/timeline_event_layout.dart';
+import 'package:commet/ui/molecules/user_panel.dart';
+import 'package:commet/ui/navigation/adaptive_dialog.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart' as material;
@@ -43,9 +46,44 @@ class _TimelineEventViewReactionsState extends State<TimelineEventViewReactions>
         direction: material.Axis.horizontal,
         children: reactions.keys.map((key) {
           var value = reactions[key]!;
+
           return EmojiReaction(
               emoji: key,
               onTapped: onReactionTapped,
+              onLongPressed: (emote) {
+                AdaptiveDialog.show(
+                  context,
+                  scrollable: false,
+                  builder: (context) => SizedBox(
+                    height: 400,
+                    width: 400,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: EmojiWidget(
+                            key,
+                            height: 40,
+                          ),
+                        ),
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: value.length,
+                            itemBuilder: (context, index) {
+                              final id = value.elementAt(index % value.length);
+                              var member =
+                                  widget.timeline.room.getMemberOrFallback(id);
+                              return MemberPanel(
+                                  member: member,
+                                  client: widget.timeline.client);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
               numReactions: value.length,
               highlighted: value.contains(currentUserIdentifier));
         }).toList());
