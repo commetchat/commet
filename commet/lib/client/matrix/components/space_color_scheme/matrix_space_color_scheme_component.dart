99a811c6359cbdd9dcb870a3cc52c39c3a230134
Airyzz <36567925+Airyzz@users.noreply.github.com>
2025-11-29 11:27:42 +0000

    Dont generate space color scheme in parallel (#511)

--- commet/lib/client/matrix/components/space_color_scheme/matrix_space_color_scheme_component.dart
+++ commet/lib/client/matrix/components/space_color_scheme/matrix_space_color_scheme_component.dart
@@ -1,6 +1,7 @@
 import 'package:commet/client/components/space_color_scheme/space_color_scheme_component.dart';
 import 'package:commet/client/matrix/matrix_client.dart';
 import 'package:commet/client/matrix/matrix_space.dart';
+import 'package:commet/utils/task_scheduler.dart';
 import 'package:flutter/src/material/color_scheme.dart';
 
 class MatrixSpaceColorSchemeComponent
@@ -14,22 +15,27 @@ class MatrixSpaceColorSchemeComponent
   @override
   MatrixSpace space;
 
+  static TaskScheduler scheduler = OneAtATimeScheduler();
+
   MatrixSpaceColorSchemeComponent(this.client, this.space) {
     _scheme = ColorScheme.fromSeed(seedColor: space.color);
-    updateColorScheme();
-    space.onUpdate.listen((_) => updateColorScheme());
+
+    MatrixSpaceColorSchemeComponent.scheduler.enqueue(updateColorScheme);
+
+    space.onUpdate.listen((_) {
+      MatrixSpaceColorSchemeComponent.scheduler.enqueue(updateColorScheme);
+    });
   }
 
   late ColorScheme _scheme;
 
-  void updateColorScheme() {
+  Future<void> updateColorScheme() async {
     if (space.avatar != null) {
-      ColorScheme.fromImageProvider(
-              provider: space.avatar!,
-              dynamicSchemeVariant: DynamicSchemeVariant.tonalSpot)
-          .then((result) {
-        _scheme = result;
-      });
+      var scheme = await ColorScheme.fromImageProvider(
+          provider: space.avatar!,
+          dynamicSchemeVariant: DynamicSchemeVariant.tonalSpot);
+
+      _scheme = scheme;
     }
   }
 }
