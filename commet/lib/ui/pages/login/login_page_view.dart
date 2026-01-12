8d9949e36495f62b1444490393fe65f1e51087f8
Airyzz <36567925+Airyzz@users.noreply.github.com>
2026-01-12 02:55:01 +0000

    New login screen background (#539)

--- commet/lib/ui/pages/login/login_page_view.dart
+++ commet/lib/ui/pages/login/login_page_view.dart
@@ -89,7 +89,7 @@ class _LoginPageViewState extends State<LoginPageView> {
       body: Stack(
         children: [
           ImageFiltered(
-            imageFilter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+            imageFilter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
             child: const StarTrailsBackground(),
           ),
           SafeArea(
