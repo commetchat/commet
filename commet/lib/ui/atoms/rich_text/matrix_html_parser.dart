284073adee8cef7e8a8cd3d6463db4b7d793c203
Airyzz <36567925+Airyzz@users.noreply.github.com>
2026-01-12 20:04:43 +1030

    handle invalid uri in href

--- commet/lib/ui/atoms/rich_text/matrix_html_parser.dart
+++ commet/lib/ui/atoms/rich_text/matrix_html_parser.dart
@@ -372,7 +372,12 @@ class LinkifyHtmlExtension extends HtmlExtension {
   InlineSpan build(ExtensionContext context) {
     if (context.node.attributes.containsKey("href")) {
       var uri = context.node.attributes["href"]!;
-      var href = Uri.parse(uri);
+      late Uri href;
+      try {
+        href = Uri.parse(uri);
+      } catch (e, s) {
+        href = Uri();
+      }
 
       if (href.host == "matrix.to") {
         var result = MatrixClient.parseMatrixLink(href);
@@ -423,10 +428,18 @@ class LinkifyHtmlExtension extends HtmlExtension {
         }
       }
 
+      late Uri destination;
+
+      try {
+        destination = Uri.parse(context.node.attributes["href"]!);
+      } catch (e, s) {
+        destination = Uri();
+      }
+
       return LinkSpan.create(context.node.text!,
           clientId: client.identifier,
           context: context.buildContext!,
-          destination: Uri.parse(context.node.attributes["href"]!));
+          destination: destination);
     }
 
     return TextSpan(
