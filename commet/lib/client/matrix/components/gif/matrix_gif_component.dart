69ea4d4df24bdddda10d45d5bc8e65aaa5fc3cc3
Airyzz <36567925+Airyzz@users.noreply.github.com>
2026-02-06 09:22:07 +0000

    Use WebP for Gif search results (#579)

--- commet/lib/client/matrix/components/gif/matrix_gif_component.dart
+++ commet/lib/client/matrix/components/gif/matrix_gif_component.dart
@@ -55,7 +55,7 @@ class MatrixGifComponent implements GifComponent<MatrixClient, MatrixRoom> {
 
       matrix.Event? replyingTo;
       var uri = await matrixRoom.client
-          .uploadContent(data, filename: "sticker", contentType: "image/gif");
+          .uploadContent(data, filename: "sticker", contentType: gif.mimeType);
 
       var content = {
         "body": gif.fullResUrl.pathSegments.last,
@@ -64,9 +64,10 @@ class MatrixGifComponent implements GifComponent<MatrixClient, MatrixRoom> {
         if (preferences.stickerCompatibilityMode)
           "chat.commet.type": "chat.commet.sticker",
         "info": {
+          "chat.commet.animated": true,
           "w": gif.x.toInt(),
           "h": gif.y.toInt(),
-          "mimetype": "image/gif"
+          "mimetype": gif.mimeType
         }
       };
 
@@ -95,10 +96,10 @@ class MatrixGifComponent implements GifComponent<MatrixClient, MatrixRoom> {
 
     var formats = result['media_formats'] as Map<String, dynamic>;
 
+    String mimeType = "image/gif";
+
     var preview =
         formats['tinygif'] ?? formats['nanogif'] ?? formats['mediumgif'];
-
-    //dynamic fullRes;
 
     var fullRes = formats['gif'];
 
@@ -107,13 +108,23 @@ class MatrixGifComponent implements GifComponent<MatrixClient, MatrixRoom> {
       fullRes = formats['mediumgif'];
     }
 
+    if (formats["webp"]['size'] < fullRes['size']) {
+      fullRes = formats["webp"];
+      mimeType = "image/webp";
+    }
+
+    if (formats["webp"]['size'] < preview['size']) {
+      preview = formats["webp"];
+    }
+
     List<dynamic> dimensions = fullRes['dims']! as List<dynamic>;
 
     return GifSearchResult(
         convertUrl(preview['url']),
         convertUrl(fullRes['url']),
         (dimensions[0] as int).roundToDouble(),
-        (dimensions[1] as int).roundToDouble());
+        (dimensions[1] as int).roundToDouble(),
+        mimeType);
   }
 
   Uri convertUrl(String url) {
