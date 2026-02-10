1b20f30a7792288f3650849b89a83bd6ce12661e
Airyzz <36567925+Airyzz@users.noreply.github.com>
2026-02-10 22:33:18 +1030

    display read receipts in timeline

--- commet/lib/ui/molecules/read_indicator.dart
+++ commet/lib/ui/molecules/read_indicator.dart
@@ -1,118 +1,55 @@
-import 'dart:async';
-
-import 'package:commet/client/components/read_receipts/read_receipt_component.dart';
-import 'package:commet/client/member.dart';
-import 'package:commet/utils/common_animation.dart';
 import 'package:flutter/material.dart';
-import 'package:implicitly_animated_list/implicitly_animated_list.dart';
-import 'package:tiamat/tiamat.dart';
+import 'package:tiamat/tiamat.dart' as tiamat;
 
 import '../../client/room.dart';
 
-class ReadIndicator extends StatefulWidget {
-  const ReadIndicator({super.key, required this.component, required this.room});
-  final ReadReceiptComponent component;
+class ReadIndicator extends StatelessWidget {
+  const ReadIndicator(
+      {required this.room, required this.users, this.onTap, super.key});
   final Room room;
-  @override
-  State<ReadIndicator> createState() => ReadIndicatorState();
-}
-
-class ReadIndicatorState extends State<ReadIndicator> {
-  late List<String> receipts;
-
-  StreamSubscription? sub;
-
-  @override
-  void initState() {
-    sub = widget.component.onReadReceiptsUpdated.listen(onUpdated);
-    receipts = widget.component.receipts;
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    sub?.cancel();
-    super.dispose();
-  }
-
-  void onUpdated(void event) {
-    setState(() {
-      receipts = widget.component.receipts;
-    });
-  }
+  final Function()? onTap;
+  final List<String> users;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        height: 20,
-        child: ShaderMask(
-          shaderCallback: (rect) {
-            return const LinearGradient(
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-              colors: [
-                Colors.purple,
-                Colors.transparent,
-              ],
-              stops: [
-                0.0,
-                0.3,
-              ],
-            ).createShader(rect);
-          },
-          blendMode: BlendMode.dstOut,
-          child: ImplicitlyAnimatedList(
-            itemData: receipts,
-            scrollDirection: Axis.horizontal,
-            reverse: true,
-            initialAnimation: false,
-            deleteDuration: Duration.zero,
-            physics: const NeverScrollableScrollPhysics(),
-            insertAnimation: (context, child, animation) {
-              return SizeTransition(
-                sizeFactor: CommonAnimations.easeOut(animation),
-                axis: Axis.horizontal,
-                child: child,
-              );
-            },
-            itemBuilder: (context, data) {
-              return SingleUserReadIndicator(
-                key: ValueKey("user_read_indicator_$data"),
-                identifier: data,
-                room: widget.room,
-              );
-            },
-          ),
-        ));
-  }
-}
+    var max = 4;
+    var diff = users.length - 3;
 
-class SingleUserReadIndicator extends StatefulWidget {
-  const SingleUserReadIndicator(
-      {required this.identifier, required this.room, super.key});
-  final String identifier;
-  final Room room;
-
-  @override
-  State<SingleUserReadIndicator> createState() =>
-      _SingleUserReadIndicatorState();
-}
-
-class _SingleUserReadIndicatorState extends State<SingleUserReadIndicator> {
-  late Member member;
-  @override
-  void initState() {
-    member = widget.room.getMemberOrFallback(widget.identifier);
-    super.initState();
+    return Material(
+      color: Colors.transparent,
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (diff > 0) tiamat.Text.labelLow("+$diff "),
+            InkWell(
+              onTap: onTap,
+              child: Stack(
+                children: [
+                  for (int i = 0; i < users.length && i < max; i++)
+                    buildEntry(i, context),
+                ],
+              ),
+            )
+          ]),
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Avatar(
-      radius: 10,
-      image: member.avatar,
-      placeholderColor: widget.room.getColorOfUser(widget.identifier),
-      placeholderText: member.displayName,
+  Widget buildEntry(int index, BuildContext context) {
+    var member = room.getMemberOrFallback(users[index]);
+    return Padding(
+      padding: EdgeInsets.fromLTRB(index.toDouble() * 8, 0, 0, 0),
+      child: tiamat.Avatar(
+        border: BoxBorder.all(
+            color: ColorScheme.of(context).surfaceContainerLow,
+            width: 2,
+            strokeAlign: 0.5),
+        radius: 8,
+        image: member.avatar,
+        placeholderColor: member.defaultColor,
+        placeholderText: member.displayName,
+      ),
     );
   }
 }
