141b0b5a4ed5898b6cf9a1e4653d28b182f003b3
Airyzz <36567925+Airyzz@users.noreply.github.com>
2026-02-16 19:29:24 +1030

    update to flutter 3.41.1

--- commet/macos/Flutter/GeneratedPluginRegistrant.swift
+++ commet/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -22,7 +22,6 @@ import media_kit_libs_macos_video
 import media_kit_video
 import package_info_plus
 import pasteboard
-import path_provider_foundation
 import screen_retriever_macos
 import shared_preferences_foundation
 import sqflite_darwin
@@ -51,7 +50,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PasteboardPlugin.register(with: registry.registrar(forPlugin: "PasteboardPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
