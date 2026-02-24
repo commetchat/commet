503c13d596f8e8c9027c13ab45304d95c464f83f
Airyzz <36567925+Airyzz@users.noreply.github.com>
2026-02-24 12:50:02 +0000

    Add custom bottom padding for onscreen keyboards on desktop (#757)

--- commet/lib/ui/pages/settings/categories/developer/developer_settings_page.dart
+++ commet/lib/ui/pages/settings/categories/developer/developer_settings_page.dart
@@ -9,6 +9,7 @@ import 'package:commet/main.dart';
 import 'package:commet/ui/navigation/navigation_utils.dart';
 import 'package:commet/ui/pages/developer/benchmarks/timeline_viewer_benchmark.dart';
 import 'package:commet/ui/pages/settings/categories/app/boolean_toggle.dart';
+import 'package:commet/ui/pages/settings/categories/app/double_preference_slider.dart';
 import 'package:commet/ui/pages/settings/categories/developer/cumulative_diagnostics_widget.dart';
 import 'package:commet/utils/background_tasks/background_task_manager.dart';
 import 'package:commet/utils/background_tasks/mock_tasks.dart';
@@ -52,7 +53,15 @@ class _DeveloperSettingsPageState extends State<DeveloperSettingsPage> {
                 title: "Disable Text Cursor Management",
                 description:
                     "As part of the implementaton for the rich text editor, we sometimes have to make automated changes to the text cursor. This disables that",
-              )
+              ),
+            DoublePreferenceSlider(
+              preference: preferences.customOnscreenKeyboardViewOffset,
+              title: "Keyboard Offset",
+              min: 0.0,
+              max: 1000,
+              description:
+                  "Amount to shift the app view up by when the onscreen keyboard is shown",
+            )
           ],
         ),
       )
@@ -144,6 +153,10 @@ class _DeveloperSettingsPageState extends State<DeveloperSettingsPage> {
               tiamat.Button(
                 text: "1280x720",
                 onTap: () => windowManager.setSize(const Size(1280, 720)),
+              ),
+              tiamat.Button(
+                text: "1280x800 (Steamdeck)",
+                onTap: () => windowManager.setSize(const Size(1280, 800)),
               ),
               tiamat.Button(
                 text: "1920x1080",
