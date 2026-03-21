b2380fda19e62e38a0d40059a78c11c6f2bdd0b3
RxmTaco <117571946+RxmTaco@users.noreply.github.com>
2026-03-21 19:06:20 +0200

    added dialog with option to remove or change banner in space settings

--- commet/lib/client/matrix/components/space_banner/matrix_space_banner_component.dart
+++ commet/lib/client/matrix/components/space_banner/matrix_space_banner_component.dart
@@ -48,5 +48,18 @@ class MatrixSpaceBannerComponent
   }
 
   @override
+  Future<void> removeBanner() async {
+    await client.matrixClient.setRoomStateWithKey(
+      space.matrixRoom.id,
+      key,
+      '',
+      {
+        'url': '',
+      },
+    );
+    space.notifyUpdate();
+  }
+
+  @override
   bool get canEditBanner => space.matrixRoom.canChangeStateEvent(key);
 }
--- commet/lib/ui/pages/settings/categories/space/space_appearance_settings_page.dart
+++ commet/lib/ui/pages/settings/categories/space/space_appearance_settings_page.dart
@@ -8,6 +8,12 @@ import 'package:commet/utils/picker_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:tiamat/tiamat.dart' as tiamat;
 
+enum _BannerAction { 
+  change, 
+  remove, 
+  cancel 
+}
+
 class SpaceAppearanceSettingsPage extends StatefulWidget {
   const SpaceAppearanceSettingsPage({super.key, required this.space});
   final Space space;
@@ -76,6 +82,49 @@ class _SpaceAppearanceSettingsPageState
                 color: Colors.transparent,
                 child: InkWell(
                   onTap: () async {
+
+                    final action = await showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text("Change Banner"),
+                            content: Text("Do you want to change the banner?"),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop(_BannerAction.cancel);
+                                  },
+                                  child: Text("Cancel")),
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop(_BannerAction.remove);
+                                  },
+                                  child: Text("Remove")),
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop(_BannerAction.change);
+                                  },
+                                  child: Text("Change")),
+                            ],
+                          );
+                        });
+
+                    if (action == _BannerAction.remove) {
+                      setState(() {
+                        image = null;
+                        uploading = true;
+                      });
+                      await widget.space
+                          .getComponent<SpaceBannerComponent>()
+                          ?.removeBanner();
+                      setState(() {
+                        uploading = false;
+                      });
+                      return;
+                    } else if (action != _BannerAction.change) {
+                      return;
+                    }
+
                     var result = await PickerUtils.pickImageAndCrop(context,
                         aspectRatio: 16 / 9);
 
