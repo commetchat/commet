f522f94ffadb50cc63c40b6c7d552e806b1973b7
Airyzz <36567925+Airyzz@users.noreply.github.com>
2026-05-23 13:01:36 +0930

    Update prepare-web.sh

--- commet/scripts/prepare-web.sh
+++ commet/scripts/prepare-web.sh
@@ -14,6 +14,7 @@ rm -rf .vodozemac
 # Setup livekit web worker
 git clone https://github.com/commetchat/livekit-client-sdk-flutter.git .livekit
 cd .livekit
+git checkout hkdf
 
 dart compile js web/e2ee.worker.dart -o ../web/e2ee.worker.dart.js -m
 cd ..
