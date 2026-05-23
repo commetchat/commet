ed2e4ee94882d1536c074436deb0538e47642667
Airyzz <36567925+Airyzz@users.noreply.github.com>
2026-05-23 12:00:24 +0930

    fix web builds

--- commet/scripts/prepare-web.sh
+++ commet/scripts/prepare-web.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -ve
 
+# # Setup vodozemac
 git clone https://github.com/famedly/dart-vodozemac.git .vodozemac
 cd .vodozemac
 git checkout 0.5.0
@@ -9,3 +10,12 @@ cd ..
 rm -f ./assets/vodozemac/vodozemac_bindings_dart*
 mv .vodozemac/dart/web/pkg/vodozemac_bindings_dart* ./assets/vodozemac/
 rm -rf .vodozemac
+
+# Setup livekit web worker
+git clone https://github.com/commetchat/livekit-client-sdk-flutter.git .livekit
+cd .livekit
+
+dart compile js web/e2ee.worker.dart -o ../web/e2ee.worker.dart.js -m
+cd ..
+
+rm -rf .livekit
