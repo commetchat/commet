a63eeec05beddce4de3d86b2b16bbb8614c233ca
Airyzz <36567925+Airyzz@users.noreply.github.com>
2026-09-15 23:12:21 +0930

    Update main.cpp

--- commet/windows/runner/main.cpp
+++ commet/windows/runner/main.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include "flutter_window.h"
 #include "utils.h"
-#include "app_links/app_links_plugin_c_api.h"
 
 typedef int(__stdcall *rust_entry)();
 
@@ -36,13 +35,6 @@ int widgetRunnerEntry(std::vector<std::string> args)
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command)
 {
-
-  // Forward the link to an existing instance, if any, then exit.
-  // You may ignore the result if you need to create another window.
-  if (SendAppLinkToInstance()) {
-    return EXIT_SUCCESS;
-  }
-
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent())
