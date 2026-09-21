a575f30fee20fadf061b329a3e25cff9d2bbef3e
Airyzz <36567925+Airyzz@users.noreply.github.com>
2026-09-21 13:51:19 +0930

    fix script for compiling vodozemac on web (#1056)

--- commet/scripts/prepare-web.sh
+++ commet/scripts/prepare-web.sh
@@ -1,15 +1,16 @@
 #!/bin/sh -ve
 
 # # Setup vodozemac
-git clone https://github.com/famedly/dart-vodozemac.git .vodozemac
-cd .vodozemac
+git clone https://github.com/famedly/dart-vodozemac.git ../../.vodozemac
+cd ../../.vodozemac
 git checkout 0.5.0
 cargo install flutter_rust_bridge_codegen
 flutter_rust_bridge_codegen build-web --dart-root dart --rust-root $(readlink -f rust) --release
-cd ..
+cd ../commet/commet
+
 rm -f ./assets/vodozemac/vodozemac_bindings_dart*
-mv .vodozemac/dart/web/pkg/vodozemac_bindings_dart* ./assets/vodozemac/
-rm -rf .vodozemac
+mv ../../.vodozemac/dart/web/pkg/vodozemac_bindings_dart* ./assets/vodozemac/
+rm -rf ../../.vodozemac
 
 # Setup livekit web worker
 git clone https://github.com/commetchat/livekit-client-sdk-flutter.git .livekit
